57b1b53bf9e35a607c38431df498e722c0e2c6db
Daniel Smith <dannmasta@gmail.com>
2021-04-18 13:40:19 -0700

    update exported api

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const Parser = require('./lib/parser');
+const Resolver = require('./lib/resolver');
 const Arg = require('./lib/arg');
 
-function resolve (args, opts, locals) {
-    return new Parser.create(args, opts).resolve(locals);
-}
-
-exports = module.exports = resolve;
-exports.resolve = resolve;
+exports = module.exports = Resolver.factory();
+exports.resolve = Resolver.resolveFactory();
 exports.Arg = Arg;
-exports.Parser = Parser;
+exports.Resolver = Resolver;
